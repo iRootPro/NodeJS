a06001aace69594d7783c2cbfcffded921699db7
Alexander Neupokoev <admin@armavir.ru>
2020-10-06 16:22:36 +0300

    Dynamic parametr (id) and add another layout for course

--- models/Course.js
+++ models/Course.js
@@ -50,6 +50,11 @@ class Course {
             )
         })
     }
+
+    static async getById(id) {
+        const courses = await Course.getAll()
+        return courses.find(course => course.id === id)
+    }
 }
 
 module.exports = Course
