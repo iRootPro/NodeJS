6b6399465ab1d029b79aabd05afa00ce7192a2da
Alexander Neupokoev <admin@armavir.ru>
2020-10-22 14:38:53 +0300

    feat: cart page

--- routes/card.js
+++ routes/card.js
@@ -2,6 +2,18 @@ const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
 
+function mapCartItems(cart) {
+  return cart.items.map(c => ({
+    ...c.courseId._doc, count: c.count
+  }))
+}
+
+function computePrice(courses) {
+  return courses.reduce((acc, course) => {
+    return acc += course.price * course.count
+  }, 0)
+}
+
 router.post('/add', async (req, res) => {
   const course = await Course.findById(req.body.id)
   await req.user.addToCart(course)
@@ -14,14 +26,17 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-  // const card = await Card.fetch()
-  // res.render('card', {
-  //   title: 'Корзина',
-  //   isCard: true,
-  //   courses: card.courses,
-  //   price: card.price
-  // })
-  res.json({test: 'test'})
+  const user = await req.user
+      .populate('cart.items.courseId')
+      .execPopulate()
+
+  const courses = mapCartItems(user.cart)
+  res.render('card', {
+    title: 'Корзина',
+    isCard: true,
+    courses: courses,
+    price: computePrice(courses)
+  })
 })
 
 module.exports = router
